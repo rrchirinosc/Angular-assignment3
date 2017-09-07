346aaa7c38b0ddca638fc1bf8a643143d6e1abda
rrchirinosc <rrchirinos@gmail.com>
2017-09-07 19:08:42 +0200

    complied with assigment specs, used directive to display narroed down list

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 (function () {
     'use strict';
     
-    angular.module('NarrowMenuApp', [])
-    .controller('SelectedMenuItemsController', SelectedMenuItemsController)
-    .service('SelectedMenuItemsService', SelectedMenuItemsService)
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItems)
     .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
-    
-    
-    SelectedMenuItemsController.$inject = ['SelectedMenuItemsService'];
-    function SelectedMenuItemsController(SelectedMenuItemsService) {
-      var menu = this;
 
+    /* Directive in charge of displaying narrowed list */
+    function FoundItems() {
+      var ddo = {
+        templateUrl: 'menuList.html',
+        scope: {
+          list: '<',    // attribute pointing to narrowed list
+          onRemove: '&' // reference attribute that should point to removeItem in controller
+        }
+      };
+      return ddo;
+    }
+    
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+      var menu = this;
+      menu.name="narrow";
+
+      /* funtion invoked by search narrowing button that fetches
+         whole list, narrows it down accordingly and saves resulting list */ 
       menu.populateList = function(searchedFor) {
-        var promise = SelectedMenuItemsService.getMenuItems();
+        var promise = MenuSearchService.getMenuItems();
         menu.items = [];
         menu.selected = [];        
-        
-          promise.then(function (response) {
+
+        promise.then(function (response) {
             menu.items = response.data.menu_items;
-            menu.selected = getSelected(menu.searchFor);
+            menu.selected = getMatchedMenuItems(menu.searchFor);
             //console.log(menu.items, menu.items.length);
             //console.log(menu.searchFor);
           })
@@ -27,31 +42,38 @@
           });    
       };  
       
+      /* Removes item from displayed list */
       menu.removeItem = function(index) {
         if(index >= 0 && index < menu.selected.length)
           menu.selected.splice(index, 1);
       }
       
-      function getSelected(searchedFor)
-      {
+      /* Returns a list of items that contain the searchFor attribute
+         in their description */
+      function getMatchedMenuItems(searchedFor) {
         var len = menu.items.length;
         var selected = [];
-        var searched = searchedFor.toLowerCase();
-        for(var i = 0; i < len; i++)
-        {
-          if(menu.items[i].description.toLowerCase().indexOf(searched) !== -1)
-            selected.push(menu.items[i]);
+
+        if(searchedFor === "" || searchedFor === undefined) {
+          selected = menu.items;
+        }
+        else {
+          
+          var searched = searchedFor.toLowerCase();
+          for(var i = 0; i < len; i++)
+          {
+            if(menu.items[i].description.toLowerCase().indexOf(searched) !== -1)
+              selected.push(menu.items[i]);
+          }
         }  
 
         return selected;     
       }
-    }
-
-
+    }    
     
-    
-    SelectedMenuItemsService.$inject = ['$http', 'ApiBasePath'];
-    function SelectedMenuItemsService($http, ApiBasePath) {
+    /* Remote service that returns the menu */
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
       var service = this;
     
       service.getMenuItems = function () {
@@ -64,5 +86,5 @@
       };
     }
     
-    })();
+})();
     
